afa0c31360865da742578c4ef8680e9203767f0c
Celina Takeda <takedacarina@gmail.com>
2022-01-13 12:03:44 -0300

    commit

--- ifood/src/routes.js
+++ ifood/src/routes.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/native-stack'
+//import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/native-stack';
 
 //import { NavigationContainer } from '@react-navigation/native';
 //import { createStackNavigator } from '@react-navigation/stack';
